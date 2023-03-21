e23b7c70c7da747624bdfc03bf80e5a8134176ad
longlhhe140059 <73572102+longlhhe140059@users.noreply.github.com>
2023-03-21 16:24:09 +0700

    try some way connect mongo

--- server/db/conn.js
+++ server/db/conn.js
@@ -11,21 +11,33 @@ var _db;
 
 module.exports = {
   connectToServer: function (callback) {
-    
-    client.connect(uri,function (err, db) {
+    console.log("run pls")
+    client.connect(uri, function (err, db) {
       console.log("test");
       // Verify we got a good "db" object
       if (err) {
-        console.log('Unable to connect to the mongoDB server. Error:', err);
+        console.log("Unable to connect to the mongoDB server. Error:", err);
       } else {
         //HURRAY!! We are connected. :)
-        console.log('Connection established to', url);
+        console.log("Connection established to", url);
         // do some work here with the database
 
         //Close connection
         db.close();
       }
     });
+
+    // try{
+    //   client.connect();
+    //   const temp = client.db('mongoPractice').collection("item").findOne()
+    //    callback(console.log(temp), 1000)
+    // }catch(e){
+    //   console.log(e)
+    // }finally{
+    //   client.close();
+    // }
+    
+    
   },
 
   getDb: function () {
