3e17abdaaac5320302931e3e7c574d06690607ac
longlhhe140059 <73572102+longlhhe140059@users.noreply.github.com>
2023-03-28 17:33:26 +0700

    set up config and env

--- nodeJS-learn/server.js
+++ nodeJS-learn/server.js
@@ -1,3 +1,17 @@
-import { sum } from "./calculate.js";
-console.log("test")
-console.log("sum="+sum(1,2))
+// import { sum } from "./calculate.js";
+// console.log("test")
+// console.log("sum="+sum(1,2))
+
+//set up environment variable
+import * as dotenv from "dotenv"
+dotenv.config({ path: "./config.env" })
+
+//set up express
+import express from "express";
+const app = express();
+
+const port = process.env.PORT ?? 3000
+
+app.listen(port , async() =>{
+    console.log(`listening on port : ${port}`)
+})
